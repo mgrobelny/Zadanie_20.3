ab5adb3e447efe90c9dee4d8a625fd98015d8149
mgrobelny <michalmaciejg@gmail.com>
2017-11-26 01:14:47 +0100

    2nd commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './store';
+import DevTools from './DevTools';
 import { getCountries } from '../src/actions/actions-countries';
 
 render(
 	<Provider store={store}>
-		<h1>Inicjalizacja projektu</h1>
-		<DevTools />
+		<div>
+			<h1>Inicjalizacja projektu</h1>
+			<DevTools />
+		</div>
 	</Provider>,
 	document.getElementById('root')
 );
